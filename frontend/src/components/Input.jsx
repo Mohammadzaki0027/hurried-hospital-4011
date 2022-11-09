2310e894b7c631d23bc4fe2890f68c919d956464
pallavee <pallaveeratnakar1714@gmail.com>
2022-11-09 22:28:08 +0530

    signup_page

---
+++ frontend/src/components/Input.jsx
@@ -0,0 +1,22 @@
+import * as React from "react";
+import {Box,Input} from "@chakra-ui/react";
+// import TextField from "@mui/material/TextField";
+
+export default function FullWidthTextField({ text, handle, name }) {
+  return (
+    <Box
+      sx={{
+        width: 400,
+        maxWidth: "100%",
+      }}
+    >
+      <Input
+        fullWidth
+        placeholder={text}
+        id="fullWidth"
+        name={name}
+        onChange={handle}
+      />
+    </Box>
+  );
+}
