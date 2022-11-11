9818c4032b7cc5dd373baa0fc58c6075db3d0e08
Mohammadzaki0027 <zaki7875@gmail.com>
2022-11-12 00:34:48 +0530

    checkout page setup

--- frontend/src/Pages/Car/Checkout.jsx
+++ frontend/src/Pages/Car/Checkout.jsx
@@ -1,9 +1,12 @@
 import { Box, Button, Text } from "@chakra-ui/react";
 import React from "react";
-import Infodiv from "../../Pages/Car/Infodiv";
+
 const Checkout = () => {
   let data = localStorage.getItem("cartitem");
-
+  let citydata = localStorage.getItem("tourdata");
+  citydata = JSON.parse(citydata);
+  let from = citydata.city1;
+  let to = citydata.city2;
   let image;
   let price;
   if (data) {
@@ -41,14 +44,22 @@ const Checkout = () => {
             <h1>{price}</h1>
           </Box>
           <Box textAlign={"center"}>
-            <Button  size='md'   border='2px' width={"100px"}       bg={"#00355d"}
-            _hover={"none"}
-            color={"white"}  borderColor='green.500'>Pay</Button>
+            <Button
+              size="md"
+              border="2px"
+              width={"100px"}
+              bg={"#00355d"}
+              _hover={"none"}
+              color={"white"}
+              borderColor="green.500"
+            >
+              Book Now
+            </Button>
           </Box>
         </Box>
         {/* first div */}
 
-        <Box width={"40%"} textAlign={"center"} >
+        <Box width={"40%"} textAlign={"center"}>
           <div style={{ marginTop: "20px" }}>
             <h1>Inclusive for Free</h1>
             <h1>Collision Waiver Damage</h1>
@@ -56,6 +67,21 @@ const Checkout = () => {
             <h1>Cancellation </h1>
           </div>
         </Box>
+      </Box>
+      <Box
+        width={["90%", "90%", "70%", "70%"]}
+        border={"2px solid black"}
+        height={"auto"}
+        margin={"auto"}
+        mt={"50px"}
+        alignContent={"center"}
+        textAlign={"center"}
+      >
+        <div  style={{ marginTop: "20px" ,color:"black"}}>
+          <h1 style={{ marginTop: "20px" ,color:"black" ,fontFamily:"sans-serif",fontSize:"25px"}}>
+            From {from} to {to}
+          </h1>
+        </div>
       </Box>
     </Box>
   );
