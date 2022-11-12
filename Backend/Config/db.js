3cbef3052123470df1a702dc31af45b5a275a525
Mohammadzaki0027 <zaki7875@gmail.com>
2022-11-12 18:44:16 +0530

    backendpart

--- Backend/Config/db.js
+++ Backend/Config/db.js
@@ -3,18 +3,3 @@ require("dotenv").config()
 const url=process.env.URL
 const connection=mongoose.connect(url)
 module.exports={connection}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
--- Backend/index.js
+++ Backend/index.js
@@ -1,28 +1,18 @@
 const express=require("express")
 const { connection } = require("./Config/db")
-
 const { FlightModule } = require("./Model/flight.module")
-
 const { carRouter } = require("./Routes/Carfilter.route")
 const {userController}=require("./Routes/user.routes")
 const app=express()
-
 let cors = require('cors')
-const { CarCartRoute } = require("./Routes/CarCart.route")
-app.use(cors())
-
-
-
 require("dotenv").config()
 const PORT=process.env.PORT||8000
 app.use(express.json())
-
+app.use(cors())
+const { CarCartRoute } = require("./Routes/CarCart.route")
 
 app.use("/",CarCartRoute)
-
-
 app.use("/user",userController)
-
 app.get("/flight",async (req,res)=>{
   
     if (req.query.sort=="asc"){
