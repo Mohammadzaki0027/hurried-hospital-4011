8e18c40a1f79b75cb1c3c9e7369a9e62bddd5845
ashelake <ashelake88@gmail.com>
2022-11-12 21:09:15 +0530

    Backend and frontend done

--- Backend/index.js
+++ Backend/index.js
@@ -1,63 +1,55 @@
-const express=require("express")
-const { connection } = require("./Config/db")
+const express = require("express");
+const { connection } = require("./Config/db");
 
-const { FlightModule } = require("./Model/flight.module")
+const { FlightModule } = require("./Model/flight.module");
 
-const { carRouter } = require("./Routes/Carfilter.route")
-const {userController}=require("./Routes/user.routes")
-const app=express()
+const { carRouter } = require("./Routes/Carfilter.route");
+const { userController } = require("./Routes/user.routes");
+const app = express();
 
-let cors = require('cors')
-const { CarCartRoute } = require("./Routes/CarCart.route")
-app.use(cors())
+let cors = require("cors");
+const { CarCartRoute } = require("./Routes/CarCart.route");
+const { StayModule } = require("./Model/stay.module");
+app.use(cors());
 
+require("dotenv").config();
+const PORT = process.env.PORT || 8000;
+app.use(express.json());
 
+app.use("/", CarCartRoute);
 
-require("dotenv").config()
-const PORT=process.env.PORT||8000
-app.use(express.json())
+app.use("/user", userController);
 
+app.get("/flight", async (req, res) => {
+  if (req.query.sort == "asc") {
+    const result = await FlightModule.find().sort({ price: 1 });
+    res.send(result);
+  } else if (req.query.sort == "desc") {
+    const result = await FlightModule.find().sort({ price: -1 });
+    res.send(result);
+  } else if (req.query.sort == "min") {
+    const result = await FlightModule.find().sort({ stoptime: 1 });
+    res.send(result);
+  } else if (req.query.sort == "max") {
+    const result = await FlightModule.find().sort({ stoptime: -1 });
+    res.send(result);
+  } else {
+    const result = await FlightModule.find();
+    res.send(result);
+  }
+});
+app.get("/stay", async (req, res) => {
+  const result = await StayModule.find();
+  res.send(result);
+});
+app.use("/", carRouter);
 
-app.use("/",CarCartRoute)
-
-
-app.use("/user",userController)
-
-app.get("/flight",async (req,res)=>{
-  
-    if (req.query.sort=="asc"){
-        const result= await FlightModule.find().sort({price:1})
-        res.send(result)
-    }
-    else if(req.query.sort=="desc"){
-        const result= await FlightModule.find().sort({price:-1})
-        res.send(result)
-    }
-    else if(req.query.sort=="min"){
-        const result= await FlightModule.find().sort({stoptime:1})
-        res.send(result)
-    }
-    else if(req.query.sort=="max"){
-        const result= await FlightModule.find().sort({stoptime:-1})
-        res.send(result)
-    }
-    else{
-        const result= await FlightModule.find()
-        res.send(result)
-
-    }
-   
-})
-
-app.use("/",carRouter)
-
-app.listen(PORT,async()=>{
-    try {
-        console.log(`connected ${PORT}`);
-        await connection
-        console.log("connected")
-      
-    } catch (error) {
-        console.log(error);
-    }
-})
+app.listen(PORT, async () => {
+  try {
+    console.log(`connected ${PORT}`);
+    await connection;
+    console.log("connected");
+  } catch (error) {
+    console.log(error);
+  }
+});
