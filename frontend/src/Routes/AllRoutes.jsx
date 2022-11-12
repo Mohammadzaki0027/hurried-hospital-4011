7716cdd276f3c33fe7fdb197be0cc42130cab0e7
Mohammadzaki0027 <zaki7875@gmail.com>
2022-11-12 16:25:45 +0530

    payment page work starting

--- frontend/src/Routes/AllRoutes.jsx
+++ frontend/src/Routes/AllRoutes.jsx
@@ -1,36 +1,15 @@
 import React from "react";
-
 import Car from "../Pages/Car/Car";
-
 import Flight from "../Flight/Flight";
 import { FlightCheckout } from "../Flight/FlightCheckout";
-
-
-
-
-
-
-
-
-import { Route, Routes } from 'react-router-dom'
-import Login from './Login'
-import { Signup } from './Signup'
-
-
-
-
-
-import Homepage from './Homepage';
-
-import StayPage from './StayPage';
-import FlightPage from './FlightPage';
-import CarPage from './CarPage';
-
-import { FlightPayment } from '../Flight/FlightPayment';
-
-
-
-
+import { Route, Routes } from "react-router-dom";
+import Login from "./Login";
+import { Signup } from "./Signup";
+import Homepage from "./Homepage";
+import StayPage from "./StayPage";
+import FlightPage from "./FlightPage";
+import CarPage from "./CarPage";
+import { FlightPayment } from "../Flight/FlightPayment";
 import Checkout from "../Pages/Car/Checkout";
 
 const AllRoutes = () => {
@@ -38,30 +17,15 @@ const AllRoutes = () => {
     <Routes>
       <Route path="/login" element={<Login />} />
       <Route path="/signup" element={<Signup />} />
-
-
-
-    <Route path='/login' element={<Login/>} />
-    <Route path='/signup' element={<Signup/>}/>
-    
-    
-
-
-        <Route path="/" element={<Homepage/>}/>
-
-        <Route path="/car" element={<Car/>}/>
-
-
-        <Route path='/flight' element={<Flight/>}/>
-        <Route path="/flightcheckout" element={<FlightCheckout/>} />
-        <Route path='/flightpayment' element={<FlightPayment/>}/>
-
-
+      <Route path="/login" element={<Login />} />
+      <Route path="/signup" element={<Signup />} />
       <Route path="/" element={<Homepage />} />
-
-
       <Route path="/car" element={<Car />} />
-
+      <Route path="/flight" element={<Flight />} />
+      <Route path="/flightcheckout" element={<FlightCheckout />} />
+      <Route path="/flightpayment" element={<FlightPayment />} />
+      <Route path="/" element={<Homepage />} />
+      <Route path="/car" element={<Car />} />
       <Route path="/flight" element={<Flight />} />
       <Route path="/flightcheckout" element={<FlightCheckout />} />
       <Route path="/carcheckout" element={<Checkout />} />
